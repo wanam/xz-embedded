4785d20d81046e2a818918edad986c385f0589fb
Wanam <karamimed@gmail.com>
2015-10-02 12:08:21 +0000

    enable arm and crc64 mode

--- xz_config.h
+++ xz_config.h
@@ -11,13 +11,14 @@
 #define XZ_CONFIG_H
 
 /* Uncomment to enable CRC64 support. */
-/* #define XZ_USE_CRC64 */
+#define XZ_USE_CRC64 1
+#define XZ_DEC_ANY_CHECK 1
 
 /* Uncomment as needed to enable BCJ filter decoders. */
 /* #define XZ_DEC_X86 */
 /* #define XZ_DEC_POWERPC */
 /* #define XZ_DEC_IA64 */
-/* #define XZ_DEC_ARM */
+#define XZ_DEC_ARM
 /* #define XZ_DEC_ARMTHUMB */
 /* #define XZ_DEC_SPARC */
 
